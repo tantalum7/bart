5acaf91e6c7cc698641a4282bb819855f9150d78
Oliver <oliver@tantalum7.com>
2018-05-16 23:14:33 +0100

    basic price cache process working, pulling prices

--- scratch.py
+++ scratch.py
@@ -1,5 +1,5 @@
 from trader.trader import Trader
-from v20.pricing import Price
+from trader.price_cache import LivePriceCache
 
 if __name__ == "__main__":
 
@@ -7,18 +7,22 @@ if __name__ == "__main__":
 
     trader = Trader()
 
-    stream = trader.test_stream("XAU_CAD,NZD_SGD,CAD_SGD,USD_NOK")
+    lpc = LivePriceCache(trader._cfg, {"XAU_CAD": ["closeoutBid"], "NZD_SGD": ["closeoutBid"]})
 
     """
+    stream = trader.test_stream("XAU_CAD,NZD_SGD,CAD_SGD,USD_NOK")
     for x in range(100):
         g = next(stream)
         if isinstance(g[1], Price):
             print("{}: {}".format(g[1].instrument, g[1].closeoutBid))
         time.sleep(0.25)
-    """
+    
 
     for x in range(100):
         print("{}: {}".format(g[1].instrument, g[1].closeoutBid))
 
-        
+    """
+
+    while True:
+        pass
     print("done")
--- trader/price_cache.py
+++ trader/price_cache.py
@@ -1,6 +1,11 @@
 
 # Library imports
 import numpy as np
+import multiprocessing as mp
+import ctypes
+import time
+import v20
+from v20.pricing import Price
 
 
 class NumpyFixedFifo(object):
@@ -20,28 +25,55 @@ class LivePriceCache(object):
 
     # Start a thread to get prices 
 
-    def __init__(self, instrument_list):
-        self._instruments = set(instrument_list)
+    def __init__(self, cfg_dict, instrument_dict, cache_size=100):
+        self._dpoint_count = 0
+        self._cfg = cfg_dict
 
-    def subscribe_instrument(self, instrument):
-        self._instruments.add(instrument)
+        self._init_shared_arrays(instrument_dict, cache_size)
 
-    def unsubscribe_instrument(self, instrument):
-        self._instruments.remove(instrument)
+        self._process = _LivePriceCacheProcess(cfg_dict, self._instrument_arrays)
 
-    def update_pricing(self):
-        pass
-    def stuf(self):
-        return self._streaming_api.pricing.stream(self.active_account, instruments=instrument).parts()
+        self._process.start()
+
+    @property
+    def dataset_memory(self):
+        return self._dpoint_count * 32 * 4
+
+    def _init_shared_arrays(self, instrument_dict, cache_size):
+        self._instrument_arrays = {}
+        for instrument, fields in instrument_dict.items():
+            field_arrays = {}
+            for field in fields:
+                field_arrays[field] = (mp.Array(ctypes.c_float, cache_size), mp.Array(ctypes.c_float, cache_size))
+                self._dpoint_count += 2 * cache_size
+            self._instrument_arrays[instrument] = field_arrays
 
 
 
 
+class _LivePriceCacheProcess(mp.Process):
 
+    def __init__(self, cfg_dict, instrument_arrays):
+        super(_LivePriceCacheProcess, self).__init__()
 
+        self.cfg = cfg_dict
+        self.instrument_arrays = instrument_arrays
 
-if __name__ == "__main__":
+    def run(self):
 
-    fifo = NumpyFixedFifo(10)
+        print("process up")
 
-    print("done")
+        api = v20.Context(hostname=self.cfg["streaming_hostname"],
+                          port=self.cfg["port"],
+                          token=self.cfg["token"])
+
+        instrument_str_list = ",".join(self.instrument_arrays.keys())
+
+        stream = api.pricing.stream(self.cfg["active_account"], instruments=instrument_str_list).parts()
+
+        while True:
+
+            g = next(stream)
+            if isinstance(g[1], Price):
+                print("{}: {}".format(g[1].instrument, g[1].closeoutBid))
+            time.sleep(0.25)
